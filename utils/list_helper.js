d444fd7ce7dad14a49cfb954ba2248413ddb6814
nexumos <nexumos@gmail.com>
2023-09-02 13:37:57 -0400

    configured prettier globally with vs code extension, and added .prettierignore

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,12 @@
 const dummy = (blogs) => {
-    return 1
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
-}
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
 
 module.exports = {
-    dummy,
-    totalLikes
-}
+  dummy,
+  totalLikes,
+};
