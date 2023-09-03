2d55d72c3d5fa47b67b510219b304052a7baebc1
nexumos <nexumos@gmail.com>
2023-09-03 07:42:12 -0400

    Completed through exercise 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -35,6 +35,28 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
+test("blogs have an 'id' property", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body[0].id).toBeDefined();
+});
+
+test("blogs can be posted", async () => {
+  const blogToPost = [
+    {
+      title: "My very new blog",
+      author: "Ricardo",
+      url: "www.blog.com",
+      likes: 0,
+    },
+  ];
+
+  const numBlogsBefore = (await api.get("/api/blogs")).body.length;
+  await api.post("/api/blogs").send(blogToPost);
+  const numBlogsAfter = (await api.get("/api/blogs")).body.length;
+  expect(numBlogsAfter).toBe(numBlogsBefore + 1);
+});
+
 test("there are two blogs", async () => {
   const response = await api.get("/api/blogs");
 
