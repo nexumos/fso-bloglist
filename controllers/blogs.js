031c0516684ae048ee000c389d887ac9ec6761ab
nexumos <nexumos@gmail.com>
2023-09-04 15:05:29 -0400

    completed exercise 5.8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -14,7 +14,7 @@ const getTokenFrom = (request) => {
 };
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1 });
   response.json(blogs);
 });
 
